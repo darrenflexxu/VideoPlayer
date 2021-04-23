876be5258c1edd59588b6b8b2fcd510af46552c0
DarrenFlex Xu <darrenflexxu@users.noreply.github.com>
2021-04-23 21:36:31 +0800

    创建自定义窗体。

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <QtGui>
+#include <QtWidgets>
+#include <QtQml>
+
+#include "MainWindow.h"
+#include "WindowFramelessHelper.h"
+
+int main(int argc, char *argv[]) {
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication a(argc, argv);
+#if 0
+    qmlRegisterType<WindowFramelessHelper>("QtShark.Window", 1, 0, "FramelessHelper");
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral(":/qml/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+#endif
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
