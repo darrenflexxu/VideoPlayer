78cbb3b99a4fe4c6d6eabca24a9e07018f2e1643
DarrenFlex Xu <darrenflexxu@users.noreply.github.com>
2021-04-24 19:20:15 +0800

    添加Video播放器模块。

--- main.cpp
+++ main.cpp
@@ -1,26 +1,66 @@
-#include <QtGui>
-#include <QtWidgets>
-#include <QtQml>
-#include <QTranslator>
+﻿
+/**
+ * 叶海辉
+ * QQ群121376426
+ * http://blog.yundiantech.com/
+ */
+
+#include <QApplication>
+#include <QTextCodec>
+
+#include <QDebug>
+
+#include "AppConfig.h"
 #include "MainWindow.h"
-#include "WindowFramelessHelper.h"
 
-int main(int argc, char *argv[]) {
-    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#undef main
+int main(int argc, char *argv[])
+{
+//    bool userSoftWareOpenGL = false; //使用软件模拟openGL
+
+//    for (int i=0;i<argc;i++)
+//    {
+//        QString str = QString(argv[i]);
+
+//        if (str == "usesoftopengl")
+//        {
+//            userSoftWareOpenGL = true;
+//        }
+//        else if (str == "harddecoder")
+//        {
+//            AppConfig::gVideoHardDecoder = true;
+//        }
+//        qDebug()<<__FUNCTION__<<argv[i]<<str;
+//    }
+
+//    if (userSoftWareOpenGL)
+//    {
+//        qDebug()<<__FUNCTION__<<"\n\n !!! userSoftWareOpenGL !!! \n\n";
+//        ///没有安装显卡驱动的系统需要使用软件模拟的openGL，一般是linux系统下会存在这种情况
+//        QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
+//    }
+//    else
+//    {
+//#if defined(WIN32)
+//        QCoreApplication::setAttribute(Qt::AA_UseOpenGLES); //解决windows下使用独立显卡的时候 切换全屏 闪烁问题
+//#else
+
+//#endif
+//    }
+
     QApplication a(argc, argv);
-    QTranslator translator;
 
-    if (translator.load(":/translate/lang_zh.qm")) {
-        a.installTranslator(&translator);
-    }
-#if 0
-    qmlRegisterType<WindowFramelessHelper>("QtShark.Window", 1, 0, "FramelessHelper");
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral(":/qml/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
-#endif
+    QTextCodec *codec = QTextCodec::codecForName("GBK");
+    QTextCodec::setCodecForLocale(codec);
+
+    AppConfig::InitAllDataPath(); //初始化一些变量和路径信息
+    AppConfig::loadConfigInfoFromFile();
+
+    AppConfig::WriteLog(QString( "\n #############\n Version = %1 \n ##############").arg(AppConfig::VERSION_NAME));
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
+
